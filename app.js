a82ed215a898c7256ee16116aa56c054946a8b1f
Kassam Ali Asif <a.kassamali@fiters.co>
2021-09-21 15:40:28 +0200

    mongo db

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
@@ -12,6 +13,23 @@ const estatesRouter = require("./routes/estates")
 app.use('/', homepageRouter)
 app.use("/admin", estatesRouter)
 
+const CONNECTION_URL = "mongodb+srv://root:TcKbelPoLBtE859z@rbnb.ftcnl.mongodb.net/test";
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+});
+
+// mongoose.set('useFindAndModify', false);
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server running at port : ${PORT} address : http://localhost:${PORT}/`);
 });
+
+
+// 46VJqTc4rET73eBt
