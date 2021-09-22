3a0b8428568f21f02559bfb8b4b9f027831fdac4
Kassam Ali Asif <a.kassamali@fiters.co>
2021-09-22 15:45:31 +0200

    map

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 3000;
 const mongoose = require("mongoose");
+require('dotenv').config()
 
 app.set("view engine", "ejs");
 app.set("views", "./src/views");
